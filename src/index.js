de49ef48766aebed010a4f3e770a49bc76b4dc99
hagan23 <haganemmanuel23@gmail.com>
2023-06-01 16:31:29 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import './styles/main.css';
-import UI from './modules/ui.js';
+import { displayScores, getNewScore, deleteScore, refreshScores } from './modules/ui.js';
 
-document.addEventListener('DOMContentLoaded', UI.displayScores);
+document.addEventListener('DOMContentLoaded', displayScores);
 
-document.querySelector('#add-form').addEventListener('submit', (e) => {
-  // prevent default submits
+document.querySelector('#add-form').addEventListener('submit', handleFormSubmit);
+document.querySelector('#refresh').addEventListener('click', handleRefresh);
+
+function handleFormSubmit(e) {
   e.preventDefault();
 
-  UI.getNewScore();
-});
+  const playerName = document.querySelector('#name').value;
+  const playerScore = document.querySelector('#score').value;
+
+  getNewScore(playerName, playerScore);
+  deleteScore('Emmanuel hagan', 42);
+  deleteScore('Light', 67);
+  deleteScore('All', 20);
+}
+
+function handleRefresh() {
+  refreshScores();
+}
