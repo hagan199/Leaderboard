2892af0a44b9d08b5fbe32b7474dbe284ca75585
hagan23 <haganemmanuel23@gmail.com>
2023-06-03 18:26:02 +0000

    Testing

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,68 +1,68 @@
 import Score from './score.js';
 
-const SCORES_API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/u7kNvsXmXdu1EufOu2CH/scores/';
+const req = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/u7kNvsXmXdu1EufOu2CH/scores/';
 
 class UI {
   static displayScores = async () => {
-    const scores = await (await fetch(SCORES_API_URL)).json();
+    const scores = await (await fetch(req)).json();
     scores.result.forEach((score) => UI.addScoreToList(score));
-  };
+  }
 
   static getNewScore = () => {
     const user = document.querySelector('#name').value;
     const score = document.querySelector('#score').value;
-
-    // Validate empty form
+    // valitdate empty form
     if (user !== '' && score !== '') {
+      // making object of Score class
       const newScore = new Score(user, score);
 
-      // Add Score to UI
+      // add Score to UI
       UI.addScoreToList(newScore);
 
       // Add data to API
       UI.postData(user, score);
 
-      // Clear fields
+      // clear fields
       UI.clearFields();
     }
-  };
+  }
 
   static addScoreToList = (score) => {
     const list = document.querySelector('#score-list');
     const li = document.createElement('li');
+
     const span = document.createElement('span');
 
     span.innerHTML = `${score.user} : ${score.score}`;
 
     li.appendChild(span);
     list.appendChild(li);
-  };
+  }
 
   static postData = async (user, score) => {
-    const res = await fetch(SCORES_API_URL, {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({ user, score }),
-    });
-
+    const res = await fetch(req,
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ user, score }),
+      });
     const data = await res.json();
     return data.result;
-  };
+  }
 
   static clearFields = () => {
     document.querySelector('#name').value = '';
     document.querySelector('#score').value = '';
-  };
+  }
 
   static refreshScores = async () => {
     window.location.reload();
   };
 
   static deleteScore = async (user, score) => {
-    const res = await fetch(`${SCORES_API_URL}/${user}/${score}`, {
+    const res = await fetch(`${req}/${user}/${score}`, {
       method: 'DELETE',
     });
-
     const data = await res.json();
     return data;
   };
